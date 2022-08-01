53db391a325bd303bbd6d72a73778a1b961d7502
yasinkiziltas <kzltasyasin@gmail.com>
2022-08-01 17:04:18 +0300

    Form Validation

--- src/components/DemoApp-Inputs/FormExample.js
+++ src/components/DemoApp-Inputs/FormExample.js
@@ -1,36 +1,66 @@
 import React, { useState } from 'react'
 import { LOGO } from '../constants/index'
 
-
 export default function FormExample() {
+    const [formState, setFormState] = useState([
+        {
+            field: 'userName',
+            hasError: false,
+            label: 'Kullanıcı Adı',
+            isRequired: true,
+            value: "",
+        },
+        {
+            type: 'password',
+            field: 'userPassword',
+            hasError: false,
+            label: 'Şifre',
+            isRequired: true,
+            value: "",
+        }
+    ])
+
     const [formModel, setFormModel] = useState({
         userName: "",
         userPassword: ""
     })
+
     const [formErrorModel, setFormErrorModel] = useState({
         userName: false,
         userPassword: false
     })
 
     const onChange = (value, field) => {
-        setFormModel({ ...formModel, [field]: value })
+        const stateCopy = [...formState]
+        const findedIndex = formState.findIndex((item) => item.field === field)
+        stateCopy[findedIndex].value = value;
+        setFormState(stateCopy)
     }
 
     const formValidation = (e) => {
         e.preventDefault()
-        const obj = {
-            userName: formModel.userName.trim() === "",
-            userPassword: formModel.userPassword.trim() === ""
+        const stateCopy = [...formState]
+        const newArray = stateCopy.map((item) => {
+            if (item.isRequired && item.value.trim() === "") {
+                return {
+                    ...item,
+                    hasError: true
+                }
+            }
+            return {
+                ...item,
+                hasError: false
+            }
+        })
+        setFormState(newArray)
+        const isExistError = newArray.filter((item) => item.hasError)
+        if (isExistError.length > 0) {
+            alert("Form hatalı!")
         }
-        setFormErrorModel(obj)
-        if (Object.values(obj).includes(true)) {
-            alert("Hata Mevcut!")
-            return;
-        }
-        sendFormRequeest()
+        sendFormRequest()
     }
 
-    const sendFormRequeest = () => {
+    const sendFormRequest = () => {
         setTimeout(() => {
             alert('Giriş Başarılı!')
         }, 2000);
@@ -42,43 +72,33 @@ export default function FormExample() {
                 <img src={LOGO} width={250} height={250} className='mb-8 ml-6' />
 
                 <form onSubmit={formValidation} className='bg-white shadow-md rounded-xl px-8 pt-6 pb-8 mb-4 '>
-                    <div className='mb-4'>
-                        <label className='block text-gray-800 text-sm font-bold mb-4'
-                            htmlFor='userName'>Kullanıcı Adı </label>
-                        <input
-                            placeholder='Kullanıcı Adı..'
-                            value={formModel.userName}
-                            onChange={(e) => {
-                                onChange(e.target.value, 'userName')
-                            }}
-                            className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-800 focus:outline-none focus:shadow-violet-600'
-                        />
-                        {formErrorModel.userName && <p className='text-red-600 font-bold text-xs ml-3 mt-2 italic'>*Bu alan zorunludur</p>}
-                    </div>
+                    {formState.map((item) => {
+                        return (
+                            <div className='mb-4' key={item.field}>
+                                <label className='block text-gray-800 text-sm font-bold mb-4'
+                                    htmlFor="userName"> {item.label} {item.isRequired && <span className='text-red-500 font-bold'>*</span>}</label>
+                                <input
+                                    type={item.type ? item.type : "text"}
+                                    placeholder={item.label}
+                                    value={item.value}
+                                    onChange={(e) => {
+                                        onChange(e.target.value, item.field)
+                                    }}
+                                    name={item.field}
+                                    className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-800 focus:outline-none focus:shadow-violet-600'
+                                />
+                                {item.hasError && <p className='text-red-600 font-bold text-xs ml-3 mt-2 italic'>*{item.label} zorunludur.</p>}
 
-                    <div className='mb-4'>
-                        <label
-                            className='block text-gray-800 text-sm font-bold mb-4'
-                            htmlFor='password'>Parola </label>
-                        <input
-                            onChange={(e) => {
-                                onChange(e.target.value, 'userPassword')
-                            }}
-                            type={"password"}
-                            value={formModel.userPassword} placeholder="***********"
-                            className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-800 focus:outline-none focus:shadow-violet-600'
-                        />
-                        {formErrorModel.userPassword && <p className='text-red-600 font-bold text-xs ml-3 mt-2 italic'>*Bu alan zorunludur</p>}
-                    </div>
+                            </div>
+                        )
+                    })}
 
-                    <div className='mb-4 justify-center flex'>
-                        <button
-                            type="submit"
-                            onClick={() => { }}
-                            className={`bg-blue-500 rounded hover:bg-blue-600 text-white font-normal w-full`}
-                        >Gönder</button>
-                    </div>
 
+                    <button
+                        onClick={() => { }}
+                        className='flex justify-center rounded bg-blue-200 hover:bg-blue-500 w-full border border-blue-500 px-4 py-2 text-black font-bold '>
+                        Gönder
+                    </button>
                 </form>
             </div>
         </div>
